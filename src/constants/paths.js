9f2b67b046763cef88cdd3d5b09216e14e0129b3
Quy Nguyen <tuongquynguyen.159@gmail.com>
2024-07-08 23:45:54 +0700

    #Feat: Create app constants

---
+++ src/constants/paths.js
@@ -0,0 +1,5 @@
+export const DASHBOARD_PATH = "/";
+export const QUESTION_PATH = "/questions/:id";
+export const CREATE_QUESTION_PATH = "/add";
+export const LEADERBOARD_PATH = "/leaderboard";
+export const ERROR_PATH = "/error";
